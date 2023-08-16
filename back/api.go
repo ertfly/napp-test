ea2cfd10ca0d474b4c593f07c1fad0f5a11ed7dc
Eric Teixeira <ertfly@gmail.com>
2023-08-16 18:50:25 -0300

    fix

--- back/api.go
+++ back/api.go
@@ -17,5 +17,6 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/products", controllers.ProductsIndex).Methods("GET")
 	router.HandleFunc("/products", controllers.ProductsPost).Methods("POST")
+	router.HandleFunc("/products/{id}", controllers.ProductsPut).Methods("PUT")
 	log.Fatal(http.ListenAndServe("localhost:"+apiPort, router))
 }
--- back/controllers/products.go
+++ back/controllers/products.go
@@ -6,8 +6,11 @@ import (
 	"napptest/business"
 	"napptest/helpers"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func ProductsIndex(w http.ResponseWriter, r *http.Request) {
@@ -22,35 +25,7 @@ func ProductsPost(w http.ResponseWriter, r *http.Request) {
 	var row ProductRequest
 	json.NewDecoder(r.Body).Decode(&row)
 
-	if row.Sku == nil || strings.Trim(*row.Sku, " ") == "" {
-		helpers.ResponseError(w, 1, "sku is required")
-		return
-	}
-
-	if row.Name == nil || strings.Trim(*row.Name, " ") == "" {
-		helpers.ResponseError(w, 1, "name is required")
-		return
-	}
-
-	if row.StockTotal == nil {
-		helpers.ResponseError(w, 1, "stock_total is required")
-		return
-	}
-
-	if row.StockCut == nil {
-		helpers.ResponseError(w, 1, "stock_cut is required")
-		return
-	}
-
-	check, err := business.ProductsBySku(*row.Sku)
-	if err != nil {
-		log.Fatalln(err.Error())
-		helpers.ResponseError(w, 2, "internal server error")
-		return
-	}
-
-	if check.Id != nil {
-		helpers.ResponseError(w, 1, "sku already exists")
+	if !ProductsValidate(w, row) {
 		return
 	}
 
@@ -71,7 +46,82 @@ func ProductsPost(w http.ResponseWriter, r *http.Request) {
 	helpers.ResponseOk(w, res)
 }
 
+func ProductsPut(w http.ResponseWriter, r *http.Request) {
+	var row ProductRequest
+	json.NewDecoder(r.Body).Decode(&row)
+
+	record, err := business.ProductsById(mux.Vars(r)["id"])
+	if err != nil {
+		log.Fatalln(err.Error())
+		helpers.ResponseError(w, 2, "internal server error")
+		return
+	}
+
+	if record.Id == nil {
+		helpers.ResponseError(w, 1, "product not found")
+		return
+	}
+
+	row.Id = record.Id
+	if !ProductsValidate(w, row) {
+		return
+	}
+
+	updatedAt := time.Now().UTC().Format("2006-01-02 15:04:05")
+
+	record.Sku = row.Sku
+	record.Name = row.Name
+	record.PriceUnit = row.PriceUnit
+	record.UpdatedAt = &updatedAt
+
+	res := map[string]interface{}{
+		"id": *record.Id,
+	}
+	helpers.ResponseOk(w, res)
+}
+
+func ProductsValidate(w http.ResponseWriter, row ProductRequest) bool {
+	if row.Sku == nil || strings.Trim(*row.Sku, " ") == "" {
+		helpers.ResponseError(w, 1, "sku is required")
+		return false
+	}
+
+	check, err := business.ProductsBySku(*row.Sku)
+	if err != nil {
+		log.Fatalln(err.Error())
+		helpers.ResponseError(w, 2, "internal server error")
+		return false
+	}
+
+	if check.Id != nil && row.Id == nil {
+		helpers.ResponseError(w, 1, "sku already exists")
+		return false
+	}
+	if row.Id != nil && strconv.FormatInt(*check.Id, 10) != strconv.FormatInt(*row.Id, 10) {
+		helpers.ResponseError(w, 1, "sku already exists")
+		return false
+	}
+
+	if row.Name == nil || strings.Trim(*row.Name, " ") == "" {
+		helpers.ResponseError(w, 1, "name is required")
+		return false
+	}
+
+	if row.StockTotal == nil {
+		helpers.ResponseError(w, 1, "stock_total is required")
+		return false
+	}
+
+	if row.StockCut == nil {
+		helpers.ResponseError(w, 1, "stock_cut is required")
+		return false
+	}
+
+	return true
+}
+
 type ProductRequest struct {
+	Id         *int64
 	Sku        *string  `json:"sku"`
 	Name       *string  `json:"name"`
 	PriceUnit  *float64 `json:"price_unit"`
